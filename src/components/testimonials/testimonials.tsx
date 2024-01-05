0ef1df93da3784d6e174772de20d6da1b289b661
Pedro Torres <pedro.areal.torres@ing.com>
2024-01-05 16:13:50 +0100

    Improvement testimonials

--- src/components/testimonials/testimonials.tsx
+++ src/components/testimonials/testimonials.tsx
@@ -63,8 +63,8 @@ const Testimonials: React.FC = () => {
             Texto exemplificativo
           </p>
         </div>
-        <div className='mx-auto mt-16 flow-root max-w-2xl sm:mt-20 lg:mx-0 lg:max-w-none'>
-          <div className='-mt-8 sm:-mx-4 sm:columns-2 sm:text-[0] lg:columns-3'>
+        <div className='mx-auto flow-root max-w-2xl sm:mt-20 lg:mx-0 lg:max-w-none'>
+          <div className='-mt-16 sm:-mx-4 sm:columns-2 sm:text-[0] lg:columns-3'>
             {testimonials.map((testimonial) => (
               <div
                 key={testimonial.author.name}
